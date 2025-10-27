4a459834db97296e207a31b9b7da74121451751a
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-10-27 14:29:47 -0700

    2:29 PM

--- main.cpp
+++ main.cpp
@@ -24,9 +24,9 @@ int main() {
         cout<<"opened file"<<endl;
     }
     vector<string> racer1;
-    set<string> racer2;
-    list<string> racer3;
-    //changed it to 3D array, layers rows columns, 4 slices for the races, 1 row of data for the 
+    list<string> racer2;
+    set<string>racer3;
+    //changed it to 3D array, layers rows columns, 4 slices for the races, 1 row of data for 
     int data[4][1][3];
    
     string temp;
@@ -34,8 +34,8 @@ int main() {
     int timeset=0;
     int timelist=0;
 
-
-          
+        
+    for(int x=0;x<15;x++){
         // Reading race
         auto start=high_resolution_clock::now();
         while(file>>temp){
@@ -44,119 +44,20 @@ int main() {
         auto end=high_resolution_clock::now();
         auto duration=duration_cast<microseconds>(end-start);
         timevector=duration.count();
-        
+        data[0][0][0]=timevector;
         file.clear();
         file.seekg(0);
-
         start=high_resolution_clock::now();
-        while(file>>temp){
-            racer2.insert(temp);
-        }
-        end=high_resolution_clock::now();
-        duration=duration_cast<microseconds>(end-start);
-        timeset=duration.count();
+    
         
-        file.clear();
-        file.seekg(0);
-
-        start=high_resolution_clock::now();
-        while(file>>temp){
-            racer3.push_back(temp);
-        }
-        end=high_resolution_clock::now();
-        duration=duration_cast<microseconds>(end-start);
-        timelist=duration.count();
-        
-
-        //Sorting Race
-        start=high_resolution_clock::now();
-        sort(racer1.begin(),racer1.end());
-        end=high_resolution_clock::now();
-        duration=duration_cast<microseconds>(end-start);
-        timevector=duration.count();
-       
-
-        //set already sorts
-       
-        start=high_resolution_clock::now();
-        racer3.sort();
-        end=high_resolution_clock::now();
-        duration=duration_cast<microseconds>(end-start);
-        timelist=duration.count();
-        
-
-        //Inserting Race
-        int place=racer1.size()/2;
-        start=high_resolution_clock::now();
-        racer1.insert(racer1.begin()+place,"TESTCODE");
-        end=high_resolution_clock::now();
-        duration=duration_cast<microseconds>(end-start);
-        timevector=duration.count();
-        
-
-        start=high_resolution_clock::now();
-        racer2.insert("TESTCODE");
-        end=high_resolution_clock::now();
-        duration=duration_cast<microseconds>(end-start);
-        timeset=duration.count();
-
-        start=high_resolution_clock::now();
-        auto iter=racer3.begin();
-        advance(iter,racer3.size()/2);
-        racer3.insert(iter,"TESTCODE");
-        end=high_resolution_clock::now();
-        duration=duration_cast<microseconds>(end-start);
-        timelist=duration.count();
-        
-
-        //Deleting Race
-        start=high_resolution_clock::now();
-        int middle=racer1.size()/2;
-        racer1.erase(racer1.begin()+middle);
-        end=high_resolution_clock::now();
-        duration=duration_cast<microseconds>(end-start);
-        timevector=duration.count();
-        
-
-        start=high_resolution_clock::now();
-        auto it=racer2.begin();
-        advance(it,racer2.size()/2);
-        racer2.erase(it);
-        end=high_resolution_clock::now();
-        duration=duration_cast<microseconds>(end-start);
-        timeset=duration.count();
-       
-
-        start=high_resolution_clock::now();
-        auto its=racer3.begin();
-        advance(its,racer3.size()/2);
-        racer3.erase(its);
-        end=high_resolution_clock::now();
-        duration=duration_cast<microseconds>(end-start);
-        timelist=duration.count();
-        
-
+    }
         //Outputting the resulst of the races
-        cout<<left<<setw(12)<<"Operation"<<right<<setw(10)<<"Vector"
-        <<right<<setw(10)<<"Set"<<right<<setw(10)<<"List"<<endl;
-        printvector(reading,"Read");
-        printvector(sorting,"Sort");
-        printvector(inserting,"Insert");
-        printvector(deleting,"Delete");
-        cout<<endl;
+  
       
     
      
     file.close();
     return 0;
-}
-
-void printvector(vector<int>& pass,string name){
-    cout<<left<<setw(12)<<name<<" ";
-    for(int temp: pass){
-        cout<<right<<setw(10)<<temp;
-    }
-    cout<<endl;
 }
 /* syntax examples:
 auto start = high_resolution_clock::now()
