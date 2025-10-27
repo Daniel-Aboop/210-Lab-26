753a00d66b0335940958ba0d16683f55ebabd886
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-10-27 16:12:02 -0700

    4:11 PM

--- main.cpp
+++ main.cpp
@@ -33,10 +33,10 @@ int main() {
     int timevector=0;
     int timeset=0;
     int timelist=0;
-    int counter=0;
+    int counter=1;
     for(int simulation=0;simulation<3;simulation++){
         cout<<counter<<endl;
-    // Reading race
+    // Reading race-------------------------------------------------
         vector<string> racer1;
         list<string> racer2;
         set<string> racer3;
@@ -69,38 +69,79 @@ int main() {
         data[0][2][1]= data[0][2][0];
         counter++;
 
-    //Sorting Race
+    //Sorting Race--------------------------------------------
     //Vector
-      start=high_resolution_clock::now();
-      sort(racer1.begin(),racer1.end());
-      end=high_resolution_clock::now();
-      duration=duration_cast<microseconds>(end-start);
-      data[1][0][0]=duration.count();
-      data[1][0][1]= data[1][0][0];
+        start=high_resolution_clock::now();
+        sort(racer1.begin(),racer1.end());
+        end=high_resolution_clock::now();
+        duration=duration_cast<microseconds>(end-start);
+        data[1][0][0]=duration.count();
+        data[1][0][1]= data[1][0][0];
     //List
-      start=high_resolution_clock::now();
-      racer2.sort();
-      end=high_resolution_clock::now();
-      duration=duration_cast<microseconds>(end-start);
-      timelist=duration.count();
-      data[1][1][0]=duration.count();
-      data[1][2][1]+=data[1][1][0];
+        start=high_resolution_clock::now();
+        racer2.sort();
+        end=high_resolution_clock::now();
+        duration=duration_cast<microseconds>(end-start);
+        timelist=duration.count();
+        data[1][1][0]=duration.count();
+        data[1][2][1]+=data[1][1][0];
     //Set
-      data[1][2][0]=-1;
-      data[1][2][1]+=data[1][2][0];
+        data[1][2][0]=-1;
+        data[1][2][1]+=data[1][2][0];
 
-    //Inserting Race
+    //Inserting Race-------------------------
     //Vector
-     int place=racer1.size()/2;
-     start=high_resolution_clock::now();
-     racer1.insert(racer1.begin()+place,"TESTCODE");
-     end=high_resolution_clock::now();
-     duration=duration_cast<microseconds>(end-start);
-     data[2][0][0]=duration.count();
-     data[2][0][1]+=data[2][0][0];
+        int place=racer1.size()/2;
+        start=high_resolution_clock::now();
+        racer1.insert(racer1.begin()+place,"TESTCODE");
+        end=high_resolution_clock::now();
+        duration=duration_cast<microseconds>(end-start);
+        data[2][0][0]=duration.count();
+        data[2][0][1]+=data[2][0][0];
+    //List
+        start=high_resolution_clock::now();
+        auto iter=racer2.begin();
+        advance(iter,racer2.size()/2);
+        racer2.insert(iter,"TESTCODE");
+        end=high_resolution_clock::now();
+        duration=duration_cast<microseconds>(end-start);
+        data[2][1][0]=duration.count();
+        data[2][1][1]+=data[2][1][0];
     //Set
-        
-
+        start=high_resolution_clock::now();
+        racer3.insert("TESTCODE");
+        end=high_resolution_clock::now();
+        duration=duration_cast<microseconds>(end-start);
+        data[2][2][0]=duration.count();
+        data[2][1][1]+=data[2][2][0];
+    //Deleting Race-------------------------------------
+    //Vector
+        start=high_resolution_clock::now();
+        int middle=racer1.size()/2;
+        racer1.erase(racer1.begin()+middle);
+        end=high_resolution_clock::now();
+        duration=duration_cast<microseconds>(end-start);
+        data[3][0][0]=duration.count();
+        data[3][0][1]+=data[3][0][0];
+    //List
+        start=high_resolution_clock::now();
+        auto its=racer2.begin();
+        advance(its,racer2.size()/2);
+        racer2.erase(its);
+        end=high_resolution_clock::now();
+        duration=duration_cast<microseconds>(end-start);
+        data[3][1][0]=duration.count();
+        data[3][1][1]+=data[3][1][0];
+    //Set
+        start=high_resolution_clock::now();
+        auto it=racer2.begin();
+        advance(it,racer2.size()/2);
+        racer2.erase(it);
+        end=high_resolution_clock::now();
+        duration=duration_cast<microseconds>(end-start);
+        data[3][2][0]=duration.count();
+        data[3][2][1]+=data[3][2][0];
+        counter++;
     }
     //Outputting the resulst of the races
 
