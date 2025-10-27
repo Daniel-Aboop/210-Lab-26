2b35185464089e29c37f13bb4ecbe633c2d63b17
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-10-27 14:39:55 -0700

    2:39 PM

--- main.cpp
+++ main.cpp
@@ -28,15 +28,14 @@ int main() {
     set<string>racer3;
     //changed it to 3D array, layers rows columns, 4 slices for the races, 1 row of data for 
     int data[4][1][3];
-   
     string temp;
     int timevector=0;
     int timeset=0;
     int timelist=0;
-
-        
+    int counter;
     for(int x=0;x<15;x++){
         // Reading race
+        //Vector
         auto start=high_resolution_clock::now();
         while(file>>temp){
             racer1.push_back(temp);
@@ -44,15 +43,44 @@ int main() {
         auto end=high_resolution_clock::now();
         auto duration=duration_cast<microseconds>(end-start);
         timevector=duration.count();
-        data[0][0][0]=timevector;
+        data[0][0][0]+=timevector;
         file.clear();
         file.seekg(0);
+        //List
         start=high_resolution_clock::now();
-    
+        while(file>>temp){
+            racer2.push_back(temp);
+        }
+        end=high_resolution_clock::now();
+        duration=duration_cast<microseconds>(end-start);
+        timelist=duration.count();
+        data[0][0][1]+=timelist;
+        file.clear();
+        file.seekg(0);
+        //Set
+        start=high_resolution_clock::now();
+        while(file>>temp){
+            racer3.insert(temp);
+        }
+        end=high_resolution_clock::now();
+        duration=duration_cast<microseconds>(end-start);
+        timeset=duration.count();
+        data[0][0][2]+=timeset;
+        file.clear();
+        file.seekg(0);    
         
+        counter++;
     }
         //Outputting the resulst of the races
-  
+        string races[4]={"Read","Sort","Insert","Delete"};
+        cout<<"Number of simulations: "<<counter;
+        // Since I setup my 3d array the rows will always be 0 so i didnt put it in the output
+        for(int x=0;x<4;x++){
+            
+            for(int z=0;z<3;z++){
+
+            }
+        }
       
     
      
